5a1d0e087180145ba1b69e20e683c4a2fe9ffc31
Jor Bashllari <altnate2000@gmail.com>
2020-06-19 16:15:21 +0200

    allow impure /lib64/ for running small executables without `steam-run` or `patchelf`

--- hw-configuration.nix
+++ hw-configuration.nix
@@ -12,6 +12,13 @@ in
         ./laptop-configuration.nix)
   ];
 
+  # https://discourse.nixos.org/t/runtime-alternative-to-patchelf-set-interpreter/3539/5
+  system.activationScripts.ldso = lib.stringAfter [ "usrbinenv" ] ''
+    mkdir -m 0755 -p /lib64
+    ln -sfn ${pkgs.glibc.out}/lib64/ld-linux-x86-64.so.2 /lib64/ld-linux-x86-64.so.2.tmp
+    mv -f /lib64/ld-linux-x86-64.so.2.tmp /lib64/ld-linux-x86-64.so.2 # atomically replace
+  '';
+
   fileSystems = lib.mkDefault {
     "/" = {
       device = "/dev/disk/by-label/lunix";
